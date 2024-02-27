1b252c77089a9241e3268b423809f67ebf593e46
andriusbern <andrius.bernatavicius@gmail.com>
2024-02-27 12:37:05 +0100

    fixed embedding loader reliance on training files

--- setup.sh
+++ setup.sh
@@ -17,6 +17,16 @@ rm XL.tar
 cd ../..
 
 ## Test the installation
-printf "\n\nTesting the installation... Generating SMILES for P29275 (A2AR_HUMAN) using the pretrained model."
+printf "\n\nTesting the installation... Generating SMILES for P29275 (A2AR_HUMAN) using the pretrained model.\n"
 
 python pcmol/generate.py --target P29275
+
+printf "**************"
+printf "\n\nInstallation complete. You can now use the package by activating the conda environment using \n\tconda activate pcmol"
+printf "\n\nTo generate SMILES for a target protein, use the following command:"
+printf "\n\tpython pcmol/generate.py --target <accession_ID>"
+printf "\n\nFor example, to generate SMILES for P29275 (A2AR_HUMAN), use the following command:"
+printf "\n\tpython pcmol/generate.py --target P29275"
+
+printf "\n\n*If no GPU is available, the generation process may take a long time. Additional flag must be used when generating without GPU:"
+printf "\n\tpython pcmol/generate.py --target P29275 --device cpu\n"
